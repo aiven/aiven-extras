cb44b8c86f076aa573858ebe3cfbca823c7afee1
Hannu Valtonen <hannu.valtonen@aiven.io>
2018-12-03 13:43:06 +0200

    aiven_extras: Add function for switching replication role

--- sql/aiven_extras.sql
+++ sql/aiven_extras.sql
@@ -93,3 +93,19 @@ CREATE FUNCTION pg_list_all_subscriptions()
 RETURNS SETOF pg_subscription LANGUAGE sql AS $$
     SELECT * FROM pg_catalog.pg_subscription;
 $$ SECURITY DEFINER;
+
+
+--Adapted from comment by C. Keane at https://forums.aws.amazon.com/thread.jspa?messageID=561509#jive-message-527767
+
+CREATE OR REPLACE FUNCTION session_replication_role(
+    role TEXT
+)
+RETURNS TEXT LANGUAGE plpgsql AS $$
+DECLARE
+    curr_val text := 'unset';
+BEGIN
+    EXECUTE 'SET session_replication_role = ' || quote_literal(role);
+    EXECUTE 'SHOW session_replication_role' INTO curr_val;
+    RETURN curr_val;
+END
+$$ SECURITY DEFINER;
