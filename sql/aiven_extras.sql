49cb9b3b962db8c7b537841df136424a11a9cc49
Rauli Ikonen <rauli@aiven.io>
2020-03-25 13:08:40 +0200

    Add support for subscription disable, enable and refresh publication

--- sql/aiven_extras.sql
+++ sql/aiven_extras.sql
@@ -56,6 +56,39 @@ END;
 $$ SECURITY DEFINER;
 
 
+CREATE FUNCTION pg_alter_subscription_disable(
+    arg_subscription_name TEXT
+)
+RETURNS VOID LANGUAGE plpgsql AS $$
+BEGIN
+    EXECUTE format('ALTER SUBSCRIPTION %I DISABLE', arg_subscription_name);
+END;
+$$ SECURITY DEFINER;
+
+
+CREATE FUNCTION pg_alter_subscription_enable(
+    arg_subscription_name TEXT
+)
+RETURNS VOID LANGUAGE plpgsql AS $$
+BEGIN
+    EXECUTE format('ALTER SUBSCRIPTION %I ENABLE', arg_subscription_name);
+END;
+$$ SECURITY DEFINER;
+
+
+CREATE FUNCTION pg_alter_subscription_refresh_publication(
+    arg_subscription_name TEXT,
+    arg_copy_data BOOLEAN = TRUE
+)
+RETURNS VOID LANGUAGE plpgsql AS $$
+BEGIN
+    EXECUTE format(
+        'ALTER SUBSCRIPTION %I REFRESH PUBLICATION WITH (copy_data=%s)',
+        arg_subscription_name, arg_copy_data::TEXT);
+END;
+$$ SECURITY DEFINER;
+
+
 CREATE FUNCTION pg_drop_subscription(
     arg_subscription_name TEXT
 )
